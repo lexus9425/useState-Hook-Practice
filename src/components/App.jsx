2d3e4151d91ffe6ec735f323babfa6607c9a11d0
lexus9425 <68475116+lexus9425@users.noreply.github.com>
2022-08-11 11:47:16 -0400

    Final Commit -Added ability for time to update every second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,31 @@
 import React, { useState } from "react";
 
 function App() {
-  //Challenge:
-  //1. Given that you can get the current time using:
-  let time = new Date().toLocaleTimeString("en-US", {
+  setInterval(updateTime, 1000);
+
+  const time = new Date().toLocaleTimeString("en-US", {
     hour12: false,
     hour: "numeric",
     minute: "numeric",
     second: "numeric"
   });
 
-  const [currentTime, getTime] = useState(0);
+  const [currentTime, setTime] = useState(time);
 
-  //Show the latest time in the <h1> when the Get Time button
-  //is pressed.
-
-  //2. Given that you can get code to be called every second
-  //using the setInterval method.
-  //Can you get the time in your <h1> to update every second?
-
-  //e.g. uncomment the code below to see Hey printed every second.
-  // function sayHi() {
-  //   console.log("Hey");
-  // }
-  // setInterval(sayHi, 1000);
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString("en-US", {
+      hour12: false,
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric"
+    });
+    setTime(newTime);
+  }
 
   return (
     <div className="container">
       <h1>{currentTime}</h1>
-      <button onClick={() => getTime(time)}>Get Time</button>
+      <button onClick={() => updateTime()}>Get Time</button>
     </div>
   );
 }

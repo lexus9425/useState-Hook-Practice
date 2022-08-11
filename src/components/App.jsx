ddcf5b573d628dac081f8bf6f0b973bf4d8e2366
lexus9425 <68475116+lexus9425@users.noreply.github.com>
2022-08-11 11:27:16 -0400

    Challenge 1 Complete - Move all code from index.js inside function in App.jsx
    - Modify get time code to remove AM/PM
    
    - use useState() method to render time when button is clicked

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  //Challenge:
+  //1. Given that you can get the current time using:
+  let time = new Date().toLocaleTimeString("en-US", {
+    hour12: false,
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric"
+  });
+
+  const [currentTime, getTime] = useState(0);
+
+  //Show the latest time in the <h1> when the Get Time button
+  //is pressed.
+
+  //2. Given that you can get code to be called every second
+  //using the setInterval method.
+  //Can you get the time in your <h1> to update every second?
+
+  //e.g. uncomment the code below to see Hey printed every second.
+  // function sayHi() {
+  //   console.log("Hey");
+  // }
+  // setInterval(sayHi, 1000);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{currentTime}</h1>
+      <button onClick={() => getTime(time)}>Get Time</button>
     </div>
   );
 }
